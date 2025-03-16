5566016d51a1a22b4822456e0efdd6e6caaab529
Lucas <lucas.silveira630521@gmail.com>
2025-03-16 14:27:33 -0300

    Carros

---
+++ Carros.java
@@ -0,0 +1,18 @@
+public class Carros {
+    private String marca;
+    private int ano;
+    
+    public void exibir_informacoes(){
+        System.out.println("A marca do carro é " + marca);
+        System.out.println("O ano do carro é " + ano);
+    }
+    public Carros(String marca, int ano){
+        this.marca = marca;
+        this.ano = ano;
+    }
+    public static void main(String[] args) {
+        Carros meusCarros = new Carros("Bwm", 2018);
+        meusCarros.exibir_informacoes();
+    }
+   
+}
