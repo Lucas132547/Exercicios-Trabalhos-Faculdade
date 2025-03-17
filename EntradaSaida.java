6ff93477f467e9952ee934980447451ec51071cb
Lucas Faculdade <lucas.silveira630521@gmail.com>
2025-03-17 19:38:22 -0300

    Scanner

---
+++ EntradaSaida.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class EntradaSaida {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite seu nome: ");
+            String nome = scanner.nextLine();
+            System.out.println("Olá " +nome+ ", bem vindo ao Java!");
+        }
+    }
+}
