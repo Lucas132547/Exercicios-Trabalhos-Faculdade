6a9681de6cd982cb1e7db735eb4845ccdcfce0e2
Lucas Faculdade <lucas.silveira630521@gmail.com>
2025-03-17 21:12:46 -0300

    Projeto

---
+++ to_do_list_lucas_silveira/br_com_todo_main/Main.java
@@ -0,0 +1,5 @@
+package to_do_list_lucas_silveira.br_com_todo_main;
+
+public class Main {
+    
+}
---
+++ to_do_list_lucas_silveira/br_com_todo_util/Arquivo.java
@@ -0,0 +1,5 @@
+package to_do_list_lucas_silveira.br_com_todo_util;
+
+public class Arquivo {
+    
+}
