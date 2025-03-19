c282550694f5aa096ccfce45ddd3a1415e10e197
Lucas <lucas.silveira630521@gmail.com>
2025-03-19 15:31:13 -0300

    Pouca coisa

--- to_do_list_lucas_silveira/br_com_todo_main/Main.java
+++ to_do_list_lucas_silveira/br_com_todo_main/Main.java
@@ -1,5 +1,25 @@
 package to_do_list_lucas_silveira.br_com_todo_main;
 
+import java.util.Scanner;
+
 public class Main {
+    static int opcao;
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+    
+            System.out.println("############# TO DO LIST #############");
+            System.out.println("1. Adicionar tarefa");
+            System.out.println("2. Visualizar tarefas");
+            System.out.println("3. Editar tarefa");
+            System.out.println("4. Remover tarefa");
+            System.out.println("5. Marcar tarefa como concluida");
+            System.out.println("\nEscolha uma opcao: ");
+    
+            opcao = scanner.nextInt();
+            scanner.close();
+
+
+
+    }
     
 }
